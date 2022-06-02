42590836d0d78aaa4f1b055c67297daac41ff74f
CHRIS FINCH <morduun@gmail.com>
2022-06-02 11:09:55 -0400

    First commit.

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const THELENS = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ THELENS.abilities = {
+  "str": "THELENS.AbilityStr",
+  "dex": "THELENS.AbilityDex",
+  "con": "THELENS.AbilityCon",
+  "int": "THELENS.AbilityInt",
+  "wis": "THELENS.AbilityWis",
+  "cha": "THELENS.AbilityCha"
+};
+
+THELENS.abilityAbbreviations = {
+  "str": "THELENS.AbilityStrAbbr",
+  "dex": "THELENS.AbilityDexAbbr",
+  "con": "THELENS.AbilityConAbbr",
+  "int": "THELENS.AbilityIntAbbr",
+  "wis": "THELENS.AbilityWisAbbr",
+  "cha": "THELENS.AbilityChaAbbr"
+};
